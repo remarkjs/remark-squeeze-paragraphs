cbf947da12d915d4cd46180ac6f152cc3d02538a
Eugene Sharygin <eush77@gmail.com>
2015-07-22 10:07:50 +0300

    Remove empty text nodes

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
 
 module.exports = function (ast) {
   ast.children = ast.children.filter(function (node) {
-    return !(node.type == 'paragraph' && !node.children.length);
+    if (node.type == 'paragraph') {
+      // Remove empty text nodes.
+      node.children = node.children.filter(function (node) {
+        return node.type != 'text' || !/^\s*$/.test(node.value);
+      });
+
+      return node.children.length;
+    }
+
+    return true;
   });
 
   return ast;
