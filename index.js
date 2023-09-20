095c23bb9dfee729699229d6fcb75d1782ba7c64
Titus Wormer <tituswormer@gmail.com>
2023-09-20 13:16:50 +0200

    Refactor to move code to `lib/`

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-import {squeezeParagraphs} from 'mdast-squeeze-paragraphs'
-
-/**
- * @type {import('unified').Plugin<void[], import('mdast').Root>}
- */
-export default function remarkSqueezeParagraphs() {
-  return squeezeParagraphs
-}
+export {default} from './lib/index.js'
