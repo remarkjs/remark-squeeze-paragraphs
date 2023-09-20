3d9c4b5052a9440582eed74c69418396306036eb
Titus Wormer <tituswormer@gmail.com>
2023-09-20 13:18:06 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,31 +1,36 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {remark} from 'remark'
 import {u} from 'unist-builder'
 import remarkSqueezeParagraphs from './index.js'
 
-test('remark-squeeze-paragraphs', (t) => {
-  t.deepEqual(
-    remark()
-      .use(remarkSqueezeParagraphs)
-      .runSync(
-        u('root', [
-          u('paragraph', []),
-          u('paragraph', [u('text', 'first')]),
-          u('paragraph', []),
-          u('paragraph', [u('text', ''), u('text', '  \n')]),
-          u('paragraph', [
-            u('text', 'second'),
-            u('text', ' '),
-            u('text', 'value')
-          ]),
-          u('paragraph', [])
+test('remark-squeeze-paragraphs', async function (t) {
+  await t.test('should work', async function () {
+    assert.deepEqual(
+      remark()
+        .use(remarkSqueezeParagraphs)
+        .runSync(
+          u('root', [
+            u('paragraph', []),
+            u('paragraph', [u('text', 'first')]),
+            u('paragraph', []),
+            u('paragraph', [u('text', ''), u('text', '  \n')]),
+            u('paragraph', [
+              u('text', 'second'),
+              u('text', ' '),
+              u('text', 'value')
+            ]),
+            u('paragraph', [])
+          ])
+        ),
+      u('root', [
+        u('paragraph', [u('text', 'first')]),
+        u('paragraph', [
+          u('text', 'second'),
+          u('text', ' '),
+          u('text', 'value')
         ])
-      ),
-    u('root', [
-      u('paragraph', [u('text', 'first')]),
-      u('paragraph', [u('text', 'second'), u('text', ' '), u('text', 'value')])
-    ])
-  )
-
-  t.end()
+      ])
+    )
+  })
 })
